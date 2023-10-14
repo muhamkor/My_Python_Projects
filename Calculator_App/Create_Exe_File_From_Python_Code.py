3041679f7ce886f381f2d9eff6d80e1b0df3b1c9
Muhammed Kora <81500362+muhamkor@users.noreply.github.com>
2023-10-13 19:39:32 -0700

    Update Create_Exe_File_From_Python_Code.py

--- Calculator_App/Create_Exe_File_From_Python_Code.py
+++ Calculator_App/Create_Exe_File_From_Python_Code.py
@@ -2,7 +2,7 @@
 import PyInstaller.__main__
 
 
-# Use this python program to create exe files from python pgograms
+# Use this python program to create an exe files from your python pgograms
 
 PyInstaller.__main__.run([
     'Calculator.py',
