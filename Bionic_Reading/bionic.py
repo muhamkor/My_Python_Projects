57dda4ed62f146148db4bc3be2373ca5feefcc1a
Muhammed Kora <81500362+muhamkor@users.noreply.github.com>
2023-11-09 10:52:38 -0800

    Add files via upload

---
+++ Bionic_Reading/bionic.py
@@ -0,0 +1,39 @@
+# Function to make text bold using HTML tags
+def make_text_bold(text):
+	return f"**{text}**"
+
+# Open the input file and create an output file
+input_file_name = "input.txt"
+output_file_name = "output.txt"
+
+with open(input_file_name, "r", encoding= 'utf-8', errors= 'ignore') as input_file, open(output_file_name, "w") as output_file:
+	for line in input_file:
+		words = line.split()
+		bold_line = []
+
+		for word in words:
+			if len(word) == 1:
+				pass   
+			if len(word) <= 3:
+				# Make the first letter of the word bold
+				bold_word = make_text_bold(word[:1]) + word[1:]
+				bold_line.append(bold_word)
+
+			elif len(word) <= 5:
+				bold_word = make_text_bold(word[:2]) + word[2:]
+				bold_line.append(bold_word)
+
+			elif len(word) == 6:
+				bold_word = make_text_bold(word[:3]) + word[3:]
+				bold_line.append(bold_word)
+				
+			elif len(word) >= 7:
+				bold_word = make_text_bold(word[:4]) + word[4:]
+				bold_line.append(bold_word)
+			else:
+				bold_line.append(word)
+
+		# Write the modified line to the output file
+		output_file.write(" ".join(bold_line) + "\n")
+
+print("Text processing complete. Bold text saved in", output_file_name)
